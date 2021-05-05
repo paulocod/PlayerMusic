c47a06b87dfab98baf986f294e41fd76b2dab134
PauloCampos <paulo4-0@outlook.com>
2021-05-04 22:45:02 -0300

    chore: initial the final project

---
+++ data.js
@@ -0,0 +1,21 @@
+export default [
+    {
+      title:
+        "Runaway",
+      artist: "Aurora",
+      cover: "foto1.jpg",
+      file: "musica1.mp3"
+    },
+    {
+      title: "WhereMyLove",
+      artist: "SYML",
+      cover: "foto2.jpg",
+      file: "musica2.mp3"
+    },
+    {
+      title: "TheWispSings",
+      artist: "WinterAid",
+      cover: "foto3.jpg",
+      file: "musica3.mp3"
+    }
+  ];
